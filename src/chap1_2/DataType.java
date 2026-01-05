60ebcaf62055da454908b3523a09ec7a15d8ddf1
dohee-jin <doheeeq@gmail.com>
2026-01-05 17:44:02 +0900

    chap1_2: 문자 타입, 기타 지원 문법 확인

--- src/chap1_2/DataType.java
+++ src/chap1_2/DataType.java
@@ -31,6 +31,35 @@ public class DataType {
         boolean b2 = false;
         boolean b3 = 10 < 5;
 
+        // ==== 문자 타입 (char: 2byte, string) ==== //
+        char c1 = 'A'; // 2byte
+        // 컴퓨터에서 문자는 아스키코드(유니코드)로도 표현이 가능하여 변수에 정수를 넣어줘도 된다.
+        char c2 = 97;
+        char c3 = 51433; // 죩 이 출력됨
+
+        // char 는 문자 두개를 변수에 저장할 수 없다.
+
+        char[] cArr = {'h', 'e', 'l', 'l', 'o'};
+        String str = new String(cArr);
+        System.out.println("str = " + str);
+
+        // "" 안에 넣으면 문자를 알아서 배열로 바꿔준다.
+        // 아래 두 가지의 과정을 String str = ""; 한 줄로 만들 수 있다.
+        // char[] cArr = {'h', 'e', 'l', 'l', 'o'};
+        // String str = new String(cArr);
+        String str2 = "안녕하세요";
+
+        // 자바 17부터 지원하는 문법
+        // 원래는 "안녕하세요" + "ㅋㅋㅋ" 이렇게 써야 했다.
+        String str3 = """
+                안녕하세요 
+                ㅋㅋㅋ
+                """;
+
+        // 자바 9부터 지원하는 문법
+        // 동적 타이핑 지원, 자바에서는 권유하지 않음
+        var z = 100;
+        var k = "안녕";
 
     }
 }
